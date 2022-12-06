da748e0c7c333d3a42d28aa2b39d53f31d25f19c
Rohit kumar <78663043+rohit-kumar-kushwaha@users.noreply.github.com>
2022-12-06 13:03:17 +0530

    implementing server registry using Eureka server

--- ServerRegistry/src/main/java/com/serverRegistry/ServerRegistryApplication.java
+++ ServerRegistry/src/main/java/com/serverRegistry/ServerRegistryApplication.java
@@ -2,8 +2,10 @@ package com.serverRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerRegistryApplication {
 
 	public static void main(String[] args) {
